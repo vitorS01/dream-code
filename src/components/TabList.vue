c24f42c515d945caa88e6972c0f49d9eb6d6f6b0
Marcio Vitor <mvitorsf@gmail.com>
2024-11-07 22:31:14 -0300

    feat: Created tablist component

---
+++ src/components/TabList.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+  export default {
+    name: 'TabList'
+  }
+</script>
+
+<template>
+  <div class="tablist">
+    <ul>
+      <li><RouterLink to="/dashboard">Dashboard</RouterLink></li>
+      <li><RouterLink to="/problems/generate">Gere um problema</RouterLink></li>
+      <li><RouterLink to="/problems">Exercicios</RouterLink></li>
+      <li><RouterLink to="/contests">Calendário de competições</RouterLink></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+  div {
+    width: 100%;
+    height: 3rem;
+    display: flex;
+    align-items: center;
+    padding: .5rem 2rem;
+    border-radius: 2rem;
+    background-color: var(--blue-600);
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+    gap: 1.5rem;
+  }
+
+  li > a {
+    opacity: .8;
+    transition: opacity .2s ease-in;
+  }
+  li > a:hover {
+    opacity: 1;
+  }
+
+  .router-link-exact-active {
+    opacity: 1;
+  }
+</style>
