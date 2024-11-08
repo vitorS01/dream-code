02d26292f3517870ae3451ae6385f08ab8b8f196
Marcio Vitor <mvitorsf@gmail.com>
2024-11-07 22:24:02 -0300

    feat: More colors to color pallet

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
-
-import router from "./router";
+import { useUserStore } from "./stores/userStore";
 
 const app = createApp(App);
 
 const pinia = createPinia();
-
-app.use(router);
 app.use(pinia);
+
+//TODO: remember to remove mock authentication
+const userStore = useUserStore();
+userStore.login({ email: "test@example.com", id: "123", username: "vi" });
+
+import router from "./router";
+app.use(router);
 app.mount("#app");
