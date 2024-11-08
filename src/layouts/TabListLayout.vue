1f6eb05c90513868fd8d7d473752e39ab8ce0b70
Marcio Vitor <mvitorsf@gmail.com>
2024-11-07 22:30:37 -0300

    feat: Added new layout for tablist

---
+++ src/layouts/TabListLayout.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Navbar from '../components/Navbar.vue';
+  import TabList from '../components/TabList.vue';
+
+  export default {
+    name: 'MainLayout',
+    components: {
+      Navbar, TabList
+    }
+  }
+</script>
+
+<template>
+  <Navbar></Navbar>
+  <div class="main-wrapper">
+    <TabList />
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.main-wrapper {
+  margin: .5rem 4rem;
+}
+</style>
