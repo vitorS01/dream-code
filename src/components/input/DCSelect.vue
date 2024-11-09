cd2fa0a0e37fe4b9f44b87c36671962a6168ab8a
Marcio Vitor <mvitorsf@gmail.com>
2024-11-09 15:25:50 -0300

    feat: Created custom input select component

---
+++ src/components/input/DCSelect.vue
@@ -0,0 +1,94 @@
+<script lang="ts">
+import { ref, watch } from 'vue';
+
+interface Option {
+  label: string;
+  value: string;
+}
+
+export default {
+  name: 'DCSelect',
+  props: {
+    options: {
+      type: Array as () => Option[],
+      required: true
+    },
+    selected: {
+      type: String,
+      default: '',
+    }
+  },
+  setup(props, {emit}) {
+    const isOpen = ref(false);
+    const selected = ref(props.selected)
+
+    watch(selected, (newVal) => {
+      emit('update:selected', newVal);
+    })
+
+    return {isOpen, selected}
+  }
+}
+</script>
+
+<template>
+<div class="select-container">
+  <select v-model="selected" @blur="isOpen = false" @click="isOpen = !isOpen">
+    <option v-for="(option, index) in options" :key="index" :value="option.value">
+      {{ option.label }}
+    </option>
+  </select>
+  <div :class="{'flipped': isOpen}" class="icon-wrapper">
+
+    <ion-icon  name="caret-down-outline"></ion-icon>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.select-container {
+  position: relative;
+  width: max-content;
+  display: flex;
+  align-items: center;
+}
+
+select {
+  width: max-content;
+  padding: .5rem 2rem .5rem .8rem;
+  font-size: .8rem;
+  background-color: var(--blue-800);
+  border-radius: 2rem;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  text-align: left;
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+}
+select:hover {
+  opacity: .9;
+}
+select option {
+  border: none
+}
+
+.icon-wrapper {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: fit-content;
+  max-height: fit-content;
+  color: #fff;
+  right:.3rem;
+  pointer-events: none;
+  transform: rotate(0deg);
+  transition: transform .3s ease;
+}
+
+.flipped {
+  transform: rotate(180deg);
+}
+</style>
--- src/views/tablist/Generate.vue
+++ src/views/tablist/Generate.vue
@@ -1,9 +1,10 @@
 <script lang="ts">
+import DCSelect from '../../components/input/DCSelect.vue';
 import Selection from '../../components/Selection.vue';
 
   export default {
     name: 'Generate',
-    components: {Selection}
+    components: {Selection, DCSelect},
   }
 </script>
 
@@ -19,21 +20,16 @@ import Selection from '../../components/Selection.vue';
       </div>
       <form>
         <label for="context">Contexto do problema</label>
-        <textarea name="context" id="context"></textarea>
+        <textarea name="context" id="context" placeholder="Descreva uma história para o problema..." maxlength="300"></textarea>
 
-        <Selection />
-
+        <label for="selection">Tópicos</label>
+        <Selection id="selection"/>
+        
         <label for="difficulty">Dificuldade</label>
-        <select id="difficulty" name="difficulty">
-          <option value="0">Fácil</option>
-          <option value="1">Médio</option>
-          <option value="2">Difícil</option>
-        </select>
+        <DCSelect id="difficulty" name="difficulty" :options="[{label: 'Fácil', value: '0'}, {label: 'Médio', value: '1'}, {label: 'Difícil', value: '2'}]" selected="0"></DCSelect>
+
         <label for="language">Idioma</label>
-        <select id="language" name="language">
-          <option value="pt">Português</option>
-          <option value="en">Inglês</option>
-        </select>
+        <DCSelect id="language" name="language" :options="[{label: 'Português', value: 'pt'}, {label: 'Inglês', value: 'En'}]" selected="pt"></DCSelect>
 
         <button type="submit">Gerar problema</button>
       </form>
@@ -58,18 +54,72 @@ import Selection from '../../components/Selection.vue';
     background-color: var(--blue-600);
   }
 
+  .tabs > ul {
+    display: flex;
+    gap: 2rem;
+  }
+  .tabs > ul > li > a {
+    padding: .5rem 1rem;
+    border-radius: 2rem;
+    opacity: .8;
+    transition: opacity .2s ease-in;
+  }
+  .tabs > ul > li > a:hover {
+    opacity: 1;
+  }
+  .tabs > ul > li > .router-link-exact-active {
+    opacity: 1;
+    background-color: var(--primary);
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+  }
+
+  form > label {
+    font-weight: bold;
+    margin-top: .5rem;
+  }
+
+  form > textarea {
+    min-height: 6rem;
+    border: none;
+    outline: none;
+    padding: .3rem;
+    resize: none;
+    background-color: var(--blue-900);
+    border-radius: .25rem;
+  }
+
+  form > button {
+    width: max-content;
+    padding: .5rem 1rem;
+    border: none;
+    border-radius: .25rem;
+    background-color: var(--primary);
+    cursor: pointer;
+    transition: opacity .2s ease-in;
+    margin-top: .8rem;
+  }
+  form > button:hover {
+    opacity: .8;
+  }
+
   .main-content {
-    width: 60%;
+    width: 40%;
     border-radius: 2rem .5rem .5rem 2rem;
     display: flex;
     flex-direction: column;
-    gap: 1rem;
+    gap: .5rem;
+    overflow-y: auto;
     padding: 2rem;
   }
 
 
   .visualize-wrapper {
-    width: 40%;
+    width: 60%;
     border-radius: .5rem 2rem 2rem .5rem;
     padding: 2rem;
   }
