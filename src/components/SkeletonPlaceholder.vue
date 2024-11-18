f6dfa88b511cf64a432f9f900315d3ed4ebc85e3
Marcio Vitor <mvitorsf@gmail.com>
2024-11-17 23:43:42 -0300

    feat: Add skeleton placeholder component

---
+++ src/components/SkeletonPlaceholder.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+export default {
+  name: 'Skeleton',
+  props: {
+    width: {
+      type: String,
+      default: '' 
+    },
+    height: {
+      type: String,
+      default: '' 
+    },
+    borderRadius: {
+      type: String,
+      default: '' 
+    }
+  }
+}
+</script>
+
+<template>
+  <div 
+    class="placeholder" 
+    :style="{
+      width: width || 'auto', 
+      height: height || 'auto', 
+      borderRadius: borderRadius || '0'
+    }"
+  >
+    &nbsp;
+  </div>
+</template>
+
+<style scoped>
+@keyframes bgAnimate {
+  0% {
+    background-position: 50% 0;
+  }
+  100% {
+    background-position: -150% 0;
+  }
+}
+.placeholder {
+  background-image: linear-gradient(
+    to right,
+    var(--blue-800) 0%,
+    var(--primary) 10%,
+    var(--blue-800) 20%,
+    var(--blue-800) 100%
+  );
+  background-size: 200% 100%;
+  box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+  animation: bgAnimate 1.2s linear infinite;
+}
+</style>
