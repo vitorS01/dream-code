150c19dc1a606e5ecf1f4a492bb553595f7930b9
Marcio Vitor <mvitorsf@gmail.com>
2024-11-17 23:38:47 -0300

    feat: Add resizer in the problems layout

--- src/layouts/ProblemsLayout.vue
+++ src/layouts/ProblemsLayout.vue
@@ -8,6 +8,34 @@
     name: 'ProblemLayout',
     components: {
       Navbar, TabList, ProblemView, Tabs
+    },
+    data() {
+      return {
+        _resizing: false,
+        _wrapper_left: -1,
+        _wrapper_width: -1,
+        mainSize : '600px'
+      }
+    },
+    methods: {
+      handleDown() {
+        this._resizing = true;
+      },
+      handleUp() {
+        this._resizing = false;
+      },
+      handleMove(event: MouseEvent) {
+        if (!this._resizing) return;
+        this.mainSize = `${event.clientX - this._wrapper_left}px`;
+        console.log(this.mainSize)
+      }
+    },
+    mounted() {
+      const wrapper = document.querySelector('.wrapper')
+      window.onmouseup = this.handleUp;
+      window.onmousemove = this.handleMove
+      if (wrapper) this._wrapper_left = wrapper.getBoundingClientRect().left
+      if (wrapper) this._wrapper_width = wrapper.getBoundingClientRect().width
     }
   }
 </script>
@@ -16,12 +44,15 @@
   <Navbar></Navbar>
   <div class="main-wrapper">
     <TabList />
-    <div class="wrapper">
-      <div class="main-content">
+    <div class="wrapper" >
+      <div class="main-content" :style="{'width': mainSize}">
         <Tabs />
         <RouterView />
       </div>
-      <ProblemView />
+      <div class="splitter" @mousedown="handleDown">
+        <ion-icon name="remove-outline" size="large"></ion-icon>
+      </div>
+      <ProblemView :style="{'width': `${_wrapper_width - parseInt(mainSize)}px`}"/>
     </div>
   </div>
 </template>
@@ -36,16 +67,34 @@
   height: calc(100vh - 10rem);
   margin: 1rem 0;
   display: flex;
-  gap: 1rem;
 }
 
 
-.wrapper > div {
+.wrapper > div:not(.splitter) {
   background-color: var(--blue-600);
 }
 
+.splitter {
+  position: relative;
+  width: .5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 2rem;
+  transition: background-color .2s linear;
+  cursor: e-resize;
+}
+.splitter:hover {
+  background-color: var(--grey-400);
+}
+.splitter >ion-icon{
+  position: absolute;
+  opacity: .7;
+  transform: rotate(90deg);
+  width: 2rem;
+}
+
 .main-content {
-  width: 40%;
   border-radius: 2rem .5rem .5rem 2rem;
   display: flex;
   flex-direction: column;
